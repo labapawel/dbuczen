ae767a737e838887a5b82ea53ea54d6bd2d970f1
MasterWolFIX <masterwolfix@gmail.com>
2025-09-17 11:41:38 +0200

    changes2

---
+++ database/migrations/2025_09_17_092557_create_bazies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bazies', function (Blueprint $table) {
+            $table->id();
+            // dodaj pola user_id, type:int, username:string, password:str, db:str, host:str, data_wygasniacia:date
+            $table->unsignedBigInteger('user_id');
+            $table->integer('type');
+            $table->string('username')->uniqiue();
+            $table->string('password');
+            $table->string('db');
+            $table->string('host');
+            $table->date('data_wygasniacia')->nullable(); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bazies');
+    }
+};
