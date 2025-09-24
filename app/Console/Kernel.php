9ff7f11a44e6da644a4465e892c6555f23bae226
MasterWolFIX <masterwolfix@gmail.com>
2025-09-24 10:39:34 +0200

    smegiemka

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('bazy:cleanup')->everyMinute();
+        $schedule->command('bazy:cleanup')->daily();
     }
 
     protected $commands = [
